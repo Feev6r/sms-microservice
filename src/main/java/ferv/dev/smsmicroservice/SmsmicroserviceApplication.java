005d478d3dd488f864e9036fdd6dd8ff4720c773
ferv <fernandovillegass000@gmail.com>
2025-04-21 10:58:44 -0500

    proyect initialization

---
+++ src/main/java/ferv/dev/smsmicroservice/SmsmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ferv.dev.smsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ferv/dev/smsmicroservice/SmsmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ferv.dev.smsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
